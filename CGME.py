3bcc4546acb31f9a95a831b756efbc90fec29b35
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2024-07-05 12:02:04 +0900

    論文のグラフは埋め込めた

--- CGME.py
+++ CGME.py
@@ -76,25 +76,6 @@ def bfs_shortest_path(G, source_nodes, target_nodes):
     return None, float('inf')
 
 ###############################################################
-def bfs_distance_from_set(G, source_set, d):
-    distances = {node: float('inf') for node in G.nodes()}
-    queue = deque([(node, 0) for node in source_set])
-    visited = set(source_set)
-
-    while queue:
-        current_node, distance = queue.popleft()
-        if distance > d:
-            break
-        distances[current_node] = distance
-
-        for neighbor in G.neighbors(current_node):
-            if neighbor not in visited:
-                visited.add(neighbor)
-                queue.append((neighbor, distance + 1))
-
-    return distances
-
-###############################################################
 def mVG(G, nodesG, nodesH):
     # Initialize the distance table
     distance_table = defaultdict(dict)
@@ -108,7 +89,6 @@ def mVG(G, nodesG, nodesH):
             nearest_node, distance = bfs_shortest_path(subG, [target_node], phi_val)
             if target_node not in distance_table[val] or distance < distance_table[val][target_node]:
                 distance_table[val][target_node] = distance
-    # ここまで OK
 
     ### Debugging: Print the distance table
     # print("Distance Table:")
@@ -130,137 +110,28 @@ def mVG(G, nodesG, nodesH):
             break
 
     ### Debugging: Print v_star
-    print(f"v_star: {v_star}")
-    print()
+    # print(f"v_star: {v_star}")
+    # print()
 
     # Initialize xi with v_star
     xi = {v_star}
     
-    # Update xi 
-    # with vertices in the shortest path from a vertex in xi to a vertex in phi_v
-    # new_xi = set()
-    # for v in nodesG:
-    #     phi_v = get_nodes_with_val(G, G.nodes[v]['val'])
-    #     for x in xi:
-    #         for target in phi_v:
-    #             if nx.has_path(G, x, target):
-    #                 path = nx.shortest_path(G, source=x, target=target)
-    #                 new_xi.update(path)
-    # xi.update(new_xi)
-
-    # Update ξ with vertices in the shortest path from a vertex in ξ to a vertex in φ(v)
+    # Update xi with vertices in the shortest path from a vertex in xi to a vertex in phi_v
     for val in nodesH:
         phi_v = get_nodes_with_val(G, val)
         new_xi = set()
         for x in xi:
             for target in phi_v:
-                print(f"x: {x}, target: {target}")
                 # Create a subgraph for path search including nodesG, xi, and phi_v nodes
                 subG = G.subgraph(set(nodesG) | set(xi) | set(phi_v))
 
                 if nx.has_path(subG, x, target):
                     path = nx.shortest_path(subG, source=x, target=target)
-                    print(f"path: {path[1:-1]}")
-                    new_xi.update(path[1:-1])
-        print(f"new_xi: {new_xi}")
-        print()
+                    for node in path[1:-1]:
+                        if node not in phi_v:
+                            new_xi.add(node)
         xi.update(new_xi)
     
-    return xi
-
-###############################################################
-def mVG_old(G, nodesG, nodesH):
-    nodesH_G = [node for node in G.nodes() if G.nodes[node]['val'] in nodesH]
-
-    subG1_vals = {G.nodes[node]['val'] for node in nodesG}
-    subG1 = G.subgraph([node for node in G if G.nodes[node]['val'] in subG1_vals])
-
-    # ここはOK
-    distance_table = defaultdict(dict)
-    for node in nodesH_G:
-        val = G.nodes[node]['val']
-        phi_val = get_nodes_with_val(G, val)
-        for target_node in nodesG:
-            nearest_node, distance = bfs_shortest_path(G, phi_val, [target_node])
-            distance_table[node][target_node] = distance
-
-    # Debugging: Print the distance table
-    print("Distance Table:")
-    for key, value in distance_table.items():
-        print(f"{key}: {value}")
-
-    # ここも多分OK
-    v_star = []
-    for distance in range(1, len(G)):
-        for node in subG1.nodes:
-            if all(distance_table[target][node] <= distance for target in nodesH_G):
-                v_star.append(node)
-                print(f"node: {node}, distance: {distance}")
-        if v_star:
-            break
-        
-    # Debugging: Print v_star
-    print("v_star:", v_star)
-
-    xi = set(v_star)
-    for node in nodesH_G:
-        if node not in nodesG:  # nodesG に含まれない nodesH_G の頂点を対象とする
-            for target_node in v_star:
-                if nx.has_path(G, node, target_node):
-                    path = nx.shortest_path(G, source=node, target=target_node)
-                    xi.update(path)
-
-    xi &= set(nodesG)  # xi に nodesG の頂点のみを残す
-
-    return xi
-
-###############################################################
-def mVG2(G, nodesG, nodesH):
-    nodesH_G = [node for node in G.nodes() if G.nodes[node]['val'] in nodesH]
-    subG1_vals = {G.nodes[node]['val'] for node in nodesG}
-    subG1 = G.subgraph([node for node in G if G.nodes[node]['val'] in subG1_vals])
-    
-    # Initialize the distance table
-    distance_table = {node: {v: float('inf') for v in nodesG} for node in nodesH_G}
-
-    # Iterate over distances d = 1, 2, ...
-    for d in range(1, len(G)):
-        print(f"d = {d}")
-        # Set distance d to vertices whose distance from φ(v) equals d
-        for v in nodesG:
-            phi_v = get_nodes_with_val(G, G.nodes[v]['val'])
-            distances = bfs_distance_from_set(G, phi_v, d)
-            for node in nodesH_G:
-                distance_table[node][v] = distances[node]
-        
-        # Check if a vertex in subG1 has distance d from φ(v) for all v in nodes
-        v_star = None
-        for node in subG1.nodes:
-            if all(distance_table[target][node] <= d for target in nodesH_G):
-                v_star = node
-                break
-        if v_star is not None:
-            break
-    # Debugging: Print v_star
-    print("v_star:", v_star)
-    
-    # Initialize ξ with v_star
-    xi = {v_star}
-
-   # Update ξ with vertices in the shortest path from a vertex in ξ to a vertex in φ(v)
-    new_xi = set()
-    for v in nodesG:
-        phi_v = get_nodes_with_val(G, G.nodes[v]['val'])
-        for x in xi:
-            for target in phi_v:
-                if nx.has_path(G, x, target):
-                    path = nx.shortest_path(G, source=x, target=target)
-                    new_xi.update(path)
-    xi.update(new_xi)
-
-    # Keep only the nodes in xi that are in nodesG
-    xi &= set(nodesG)
-
     return xi
 
 ###############################################################
@@ -282,61 +153,40 @@ if __name__ == '__main__':
 
     G = InitializeG(n, H_nodes)
 
-    # Show_KingGraph(G)
-    
-    ###############################################################
-    # Algorithm 1 の部分実行（6 - 9 行目）
-    # vi = 0  # テストテストテスト
-    # print(f"vi = {vi}: ")
+    for vi in H_nodes:
+        ###############################################################
+        # 6 - 9 行目
+        print(f"vi = {vi}: ")
 
-    # # 6 行目
-    # for node in get_nodes_with_val(G, H_nodes[vi]):
-    #     set_node_val(G, node, '')
+        # 6 行目
+        for node in get_nodes_with_val(G, H_nodes[vi]):
+            set_node_val(G, node, '')
 
-    # # Show_KingGraph(G)
-
-    # # 7 - 9 行目
-    # for vj in H.neighbors(vi):
-    #     print(f"vj = {vj}: ", end='')
-    #     nodesH = set(H.neighbors(vj)) - {vi}
-    #     nodesG = get_nodes_with_val(G, H_nodes[vj])
-    #     print(nodesH, ", ", nodesG)
-    #     xi = mVG2(G, nodesG, nodesH)
-    #     print("xi = ", xi)
-        
-    #     for node in get_nodes_with_val(G, H_nodes[vj]):
-    #         set_node_val(G, node, '')
-        # for node in xi:
-        #     set_node_val(G, node, H_nodes[vj])
-        
         # Show_KingGraph(G)
+        
+        # 7 - 9 行目
+        for vj in H.neighbors(vi):
+            # print(f"vj = {vj}: ", end='')
+            nodesH = set(H.neighbors(vj)) - {vi}
+            nodesG = get_nodes_with_val(G, H_nodes[vj])
+            # print(nodesH, ", ", nodesG)
+            xi = mVG(G, nodesG, nodesH)
+            # print("xi = ", xi)
+            
+            for node in get_nodes_with_val(G, H_nodes[vj]):
+                set_node_val(G, node, '')
+            for node in xi:
+                set_node_val(G, node, H_nodes[vj])
+        
+            # Show_KingGraph(G)
+            
+        # 10 行目
+        nodesG = get_nodes_with_val(G, '')
+        nodesH = set(H.neighbors(vi))
+        xi = mVG(G, nodesG, nodesH)
+        # print("xi = ", xi)
     
-    ###############################################################
-    # Algorithm 1 の部分実行（10 行目）
-    for node in G.nodes():
-        set_node_val(G, node, '')
-    
-    set_node_val(G, (0, 1), 1)
-    set_node_val(G, (0, 2), 2)
-    set_node_val(G, (0, 3), 3)
-    set_node_val(G, (0, 4), 4)
-
-    set_node_val(G, (1, 2), 3)
-    set_node_val(G, (1, 3), 2)
-
-    set_node_val(G, (2, 1), 3)
-    set_node_val(G, (3, 0), 3)
-
-    # Show_KingGraph(G)
-    
-    nodesG = get_nodes_with_val(G, '')
-    print("nodesG = ", nodesG)
-    
-    nodesH = set(H.neighbors(0))
-    print("nodesH = ", nodesH)
-
-    xi = mVG(G, nodesG, nodesH)
-    print("xi = ", xi)
-    
-    subG = G.subgraph(xi)
-    # Show_KingGraph(subG)
+        for node in xi:
+            set_node_val(G, node, H_nodes[vi])
+        
+        Show_KingGraph(G)
